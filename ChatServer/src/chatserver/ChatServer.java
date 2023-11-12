db60ad3e8ce3d0f66328f115e1152c540e0bec7d
Felipe Sutter <lipeps61@gmail.com>
2023-11-12 18:08:48 -0300

    feat: server aceitando conexões e mensagens

--- ChatServer/src/chatserver/ChatServer.java
+++ ChatServer/src/chatserver/ChatServer.java
@@ -4,8 +4,12 @@
  */
 package chatserver;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  *
@@ -17,8 +21,31 @@ public class ChatServer {
 
     public static void main(String[] args) {
         try {
+            // cria o server na porta que eu disser
             ServerSocket server = new ServerSocket(PORTA);
             System.out.println("Servidor rodando na porta: " + PORTA);
+
+            while (true) {
+                // cria o socket e fica aguardando o client se conectar a ele.
+                System.out.println("Aguardando conexao...");
+                Socket client = server.accept();
+                System.out.println("Cliente" + client.getInetAddress().getHostAddress() + "conectou!");
+
+                // cria um leitor e escritor para poder ler e escrever mensagens do cliente.
+                BufferedReader leitor = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                PrintWriter escritor = new PrintWriter(client.getOutputStream());
+
+                // lê a mensagem que o client enviou
+                String mensagem = leitor.readLine();
+                System.out.println("Mensagem enviada do cliente: " + mensagem);
+
+                // enviar mensagem pro client
+                escritor.println("Sua mensagem foi recebida. Aqui está ela: " + mensagem);
+
+                client.close();
+
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
