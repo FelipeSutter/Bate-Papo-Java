4af4fb16f4e89fcbb37bb46f7946076a8d070843
Felipe Sutter <lipeps61@gmail.com>
2023-11-13 15:30:39 -0300

    Criação do clientHandler

--- ChatServer/src/chatserver/ChatServer.java
+++ ChatServer/src/chatserver/ChatServer.java
@@ -10,6 +10,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Set;
 
 /**
  *
@@ -19,32 +20,36 @@ public class ChatServer {
 
     private static final int PORTA = 1515;
 
+    private static Set<PrintWriter> escritores = new HashSet<>();
+
     public static void main(String[] args) {
-        try {
+        try (ServerSocket server = new ServerSocket(PORTA)) {
             // cria o server na porta que eu disser
-            ServerSocket server = new ServerSocket(PORTA);
             System.out.println("Servidor rodando na porta: " + PORTA);
 
             while (true) {
+
+                new ClientHandler(server.accept(), escritores).start();
+
                 // cria o socket e fica aguardando o client se conectar a ele.
-                System.out.println("Aguardando conexao...");
-                Socket client = server.accept();
-                System.out.println("Cliente " + client.getInetAddress().getHostAddress() + " conectou!");
-
-                // cria um leitor e escritor para poder ler e escrever mensagens do cliente.
-                // TODO: Fazer o Scanner para ler o input do teclado.
-                BufferedReader leitor = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                PrintWriter escritor = new PrintWriter(client.getOutputStream(), true);
-
-                // lê a mensagem que o client enviou
-                String mensagem = leitor.readLine();
-                System.out.println("Mensagem enviada do cliente: " + mensagem);
-
-                // enviar mensagem pro client
-                escritor.println("Sua mensagem foi recebida. Aqui esta ela: " + mensagem);
-
-                client.close();
-
+//                System.out.println("Aguardando conexao...");
+//                Socket client = server.accept();
+//                System.out.println("Cliente " + client.getInetAddress().getHostAddress() + " conectou!");
+//
+//                // cria um leitor e escritor para poder ler e escrever mensagens do cliente.
+//                // TODO: Fazer o Scanner para ler o input do teclado.
+//                BufferedReader leitor = new BufferedReader(new InputStreamReader(client.getInputStream()));
+//                PrintWriter escritor = new PrintWriter(client.getOutputStream(), true);
+//
+//                // lê a mensagem que o client enviou
+//                String mensagem = leitor.readLine();
+//                System.out.println("Mensagem enviada do cliente: " + mensagem);
+//
+//                // enviar mensagem pro client
+//                escritor.println("Sua mensagem foi recebida. Aqui esta ela: " + mensagem);
+//
+//                client.close();
+//;
             }
 
         } catch (IOException e) {
---
+++ ChatServer/src/chatserver/ClientHandler.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chatserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Set;
+
+/**
+ *
+ * @author FACC
+ */
+public class ClientHandler extends Thread {
+
+    private Socket socket;
+    private String username;
+    private PrintWriter escritor;
+    private Set<PrintWriter> listaDeEscritores;
+
+    ClientHandler(Socket socket, Set<PrintWriter> escritores) {
+        this.socket = socket;
+        listaDeEscritores = escritores;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader leitor = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            escritor = new PrintWriter(socket.getOutputStream(), true);
+
+            synchronized (listaDeEscritores) {
+                listaDeEscritores.add(escritor);
+            }
+
+            username = leitor.readLine();
+            transmitir(username + " entrou no chat!");
+            System.out.println(username + " conectou-se ao servidor.");
+            
+            String clientMessage;
+            do {
+                
+                clientMessage = leitor.readLine();
+                transmitir(username + ": " + clientMessage);
+                
+            } while ((clientMessage != null) || (!clientMessage.equalsIgnoreCase("/sair")));
+            
+            synchronized (listaDeEscritores) {
+                listaDeEscritores.remove(escritor);
+            }
+            
+            transmitir(username + " saiu do chat :(");
+            
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void transmitir(String mensagem) {
+        listaDeEscritores.forEach(escritor -> {
+            escritor.println(mensagem);
+        });
+    }
+
+}
