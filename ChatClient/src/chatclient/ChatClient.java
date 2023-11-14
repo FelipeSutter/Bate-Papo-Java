468b03e5039ebd535f0f890c2176689e3fc60864
Felipe Sutter <lipeps61@gmail.com>
2023-11-14 15:05:33 -0300

    Adição de interface gráfica

--- ChatClient/src/chatclient/ChatClient.java
+++ ChatClient/src/chatclient/ChatClient.java
@@ -1,69 +1,105 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package chatclient;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
 
-/**
- *
- * @author FACC
- */
-public class ChatClient {
+public class ChatClient extends javax.swing.JFrame {
 
-    public static void main(String[] args) {
+    private JTextField messageInput;
+    private JTextArea chatArea;
+    private PrintWriter writer;
+    private Socket socket;
+    
+    public ChatClient(String username) {
+        super("Chat Client");
 
-        System.out.print("Digite o seu nome: ");
-        Scanner scanner = new Scanner(System.in);
-        // pega o nome do usuario
-        String username = scanner.nextLine();
+        // Configurações da interface gráfica
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
 
-        // cria o socket onde o server está rodando, o leitor e escritor.
-        try (Socket socket = new Socket("localhost", 1515); 
-                BufferedReader leitor = new BufferedReader(new InputStreamReader(socket.getInputStream())); 
-                PrintWriter escritor = new PrintWriter(socket.getOutputStream(), true);) 
-        {
-            escritor.println(username);
+        // Componentes da interface
+        messageInput = new JTextField();
+        JButton sendButton = new JButton("Enviar");
+        chatArea = new JTextArea();
+        chatArea.setEditable(false);
 
-            // thread criada para receber mensagem dos usuários
-            Thread receptor = new Thread(() -> {
-                try {
-                    String mensagemRecebida;
-                    while((mensagemRecebida = leitor.readLine()) != null) {
-                        System.out.println(mensagemRecebida);
+        // Layout
+        setLayout(new BorderLayout());
+        add(new JScrollPane(chatArea), BorderLayout.CENTER);
+
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        bottomPanel.add(messageInput, BorderLayout.CENTER);
+        bottomPanel.add(sendButton, BorderLayout.EAST);
+        add(bottomPanel, BorderLayout.SOUTH);
+
+        // Ação do botão de enviar
+        sendButton.addActionListener(e -> sendMessage());
+
+        // Conexão com o servidor
+        try {
+            socket = new Socket("localhost", 1515);
+            writer = new PrintWriter(socket.getOutputStream(), true);
+            writer.println(username);
+
+            // Thread para receber mensagens
+            Thread receiver = new Thread(() -> {
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                    String message;
+                    while ((message = reader.readLine()) != null) {
+                        appendMessage(message);
                     }
-                } catch (IOException e2) {
-                    System.out.println(username + " saiu do chat.");
+                } catch (IOException e1) {
+                    handleDisconnect();
                 }
             });
-            
-            receptor.start();
-            
-            // cliente escrever a msg
-            // continua mandando msg se for diferente de /sair
-            String mensagem;
-            while(true){
-                Thread.sleep(300);
-                System.out.println("Escreva uma mensagem: ");
-                mensagem = scanner.nextLine();
-                if(!mensagem.isEmpty()) {
-                    escritor.println(mensagem);
-                    if(mensagem.equalsIgnoreCase("/sair")) break;
-                }
-            }
-            
-            System.out.println("Saindo do chat...");
-            receptor.interrupt();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+            receiver.start();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
 
+        // Adiciona um listener para fechar adequadamente os recursos quando a janela é fechada
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                handleDisconnect();
+            }
+        });
     }
 
+    private void sendMessage() {
+        String message = messageInput.getText().trim();
+        if (!message.isEmpty()) {
+            writer.println(message);
+            messageInput.setText("");
+        }
+    }
+
+    private void appendMessage(String message) {
+        SwingUtilities.invokeLater(() -> chatArea.append(message + "\n"));
+    }
+
+    private void handleDisconnect() {
+        try {
+            if (socket != null && !socket.isClosed()) {
+                writer.println("/sair");
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        ChatClientGUI ccf = new ChatClientGUI();
+        ccf.setVisible(true);
+        ccf.setLocationRelativeTo(ccf);
+    }
 }
